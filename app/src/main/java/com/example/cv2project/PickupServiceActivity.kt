5c4a248dc8d2833cc7f977e6893beab9dc59635d
gaa333 <brent51405140@gmail.com>
2025-02-07 14:19:28 +0900

    픽업 서비스 통합

--- app/src/main/java/com/example/cv2project/PickupServiceActivity.kt
+++ app/src/main/java/com/example/cv2project/PickupServiceActivity.kt
@@ -4,15 +4,24 @@ import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cv2project.ui.theme.CV2ProjectTheme
 
@@ -36,14 +45,28 @@ fun PickupServiceScreen() {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("픽업 서비스", fontSize = 40.sp)
-        Button(
-            onClick = {
-                context?.finish()
-            }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("뒤로")
+            Text("픽업 서비스", fontSize = 40.sp)
+            Image(
+                painter = painterResource(R.drawable.x),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 20.dp)
+                    .size(20.dp)
+                    .clickable { context?.finish() }
+            )
         }
-    }
+        Spacer(modifier = Modifier.weight(0.1f))
 
+        Image(
+            painter = painterResource(id = R.drawable.pikup),
+            contentDescription = "지도",
+            Modifier.fillMaxSize()
+        )
+        Spacer(modifier = Modifier.weight(0.15f))
+    }
 }
